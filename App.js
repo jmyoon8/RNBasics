e721e4ae7072e9d1ae03596f5fd1de251fc2a4f2
yoon <jmyoon8@naver.com>
2021-03-05 18:55:11 +0900

    강의

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import _ from'underscore'
+
+
 
 export default function App() {
+ 
+  
+  const [numbers,setNumbers]=useState([])
+  const [toggle,setToggle]=useState(true)
+  const [up,setUp]=useState(0)
+  
+  useEffect(() => {
+      
+    let result=[]
+    _.times(45,n=>result.push(n+1))
+    const number=_.shuffle(result);
+    number.length=7
+    setNumbers(number)
+  
+  }, [toggle])
+ 
+  
 
   return (
     <View style={styles.container}>
         <Text>
-          welcome React-Native
+          {numbers.join(',')}
         </Text>
+        <Text>
+            <Button onPress={()=>setToggle(!toggle)} title="조또번호"/>
+        </Text>
+
+        <Text>
+          {up}
+        </Text>
+        <Button onPress={()=>setUp(up+1)} title="증가"/>
     </View>
   );
 }
